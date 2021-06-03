17ff191dd870b61ebeadf4d9860e74176d8e89df
Yusra A <yusraa190@gmail.com>
2021-06-03 20:02:31 +0300

    initializing

---
+++ wp-custom-widget.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Plugin Name: Erza Social Profile Widget
+  Plugin URI: https://erza.com
+  Description: Links to Author social media profile
+  Author: Yusra Ahmed
+  Author URI: https://yusraahmed.com
+*/
+?>
